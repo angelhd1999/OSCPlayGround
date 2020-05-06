9e1045414ab193e1c54676513c08e707512d2586
angelhd1999 <angelhd1999@gmail.com>
2020-05-06 13:14:15 +0200

    React machine changes

---
+++ Assets/Scripts/PickUpReact.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpReact : MonoBehaviour
+{
+
+    public float reactionTime;
+    public bool activeReact = true;
+    public List<GameObject> pickUps = new List<GameObject>();
+    public Material pickUpMaterial;
+    public Material activatedPickUpMaterial;
+
+    private GameObject activatedPickUp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartReact());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void ActivatePickUp()
+    {
+        activatedPickUp = pickUps[Random.Range(0, pickUps.Count)];
+        activatedPickUp.GetComponent<MeshRenderer>().material = activatedPickUpMaterial;
+    }
+
+    private IEnumerator StartReact()
+    {
+        while (activeReact)
+        {
+            ActivatePickUp();
+            yield return new WaitForSeconds(reactionTime);
+        }
+    }
+}
